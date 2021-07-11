64b5fc1b613aecf0ed46ee880825355f422f67ee
justincremer <jacremer@live.com>
2021-07-11 00:14:10 -0700

    refactored to account for malformed urls

--- api/managed-records.js
+++ api/managed-records.js
@@ -4,40 +4,38 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 async function retrieve({ page, colors }) {
-  const default_colors = ["red", "brown", "blue", "yellow", "green"];
-  const query = query_builder(10, page * 10, colors ?? default_colors);
-  const res = await fetch_records(query);
-
-  console.log(JSON.parse(res));
+	let res = await get_records({page, colors});
+	console.log(res);
 }
 
-const fetch_records = async (query) => {
-  const base_url = "http://localhost:3000/records";
-  const url = `${base_url}?${query}`;
-  const res = await fetch(url);
-  return res;
-};
+async function get_records({ page, colors }) {
+	const allowed_colors = ["red", "brown", "blue", "yellow", "green"];
+	let valid = true;
+	
+	page = page ? page * 10 : 1;
+	if (colors) {
+		colors.foreach(c => if (!allowed_colors.contains(c)) {
+			valid = false;
+			break;
+		});
+
+		if (!valid) {
+			// handle invalid request
+			return;
+		}
+
+		const query = query_builder(10, page, colors);
+		return await fetch(`${window.path}?${query}`);
+	}
+
+	const query = query_builder(10, page, allowed_colors);
+	const url = `${window.path}?${query}`;
+	return await fetch(url);
+}
 
 const query_builder = (limit, offset, colors) =>
-  (
-    (limit ? `limit=${limit}&` : "") +
-    (offset ? `offset=${offset}&` : "") +
-    (colors ? colors.reduce((acc, i) => acc + `color[]=${i}&`, "") : "")
-  ).slice(0, -1);
-
-function test_query_builder() {
-  console.log(query_builder(2, 2, ["red", "green"]));
-  console.log(query_builder(false, 2, ["red", "green"]));
-  console.log(query_builder(false, false, ["red"]));
-  console.log(query_builder(false, 2, []));
-  console.log(query_builder(2, false, false));
-}
-
-async function test_fetch_records() {
-  await retrieve(
-    "http://localhost:3000/records",
-    query_builder(2, 2, ["red", "green"])
-  );
-}
+	  ((limit ? `limit=${limit}&` : "") +
+	   (offset ? `offset=${offset}&` : "") +
+	   (colors ? colors.reduce((acc, i) => acc + `color[]=${i}&`, "") : "")).slice(0, -1); 
 
 export default retrieve;
