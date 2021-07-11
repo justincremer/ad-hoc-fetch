9f31d87b7cf7fd18864f85f073ae4ad82e9652cb
jacremer <jacremer@live.com>
2021-07-10 23:27:44 -0700

    base retrieve method

--- api/managed-records.js
+++ api/managed-records.js
@@ -3,10 +3,20 @@ import URI from "urijs";
 
 window.path = "http://localhost:3000/records";
 
-async function retrieve(base_url, query) {
-  const res = await fetch(`${base_url}/${query}`);
-  console.log(res);
+async function retrieve({ page, colors }) {
+  const default_colors = ["red", "brown", "blue", "yellow", "green"];
+  const query = query_builder(10, page * 10, colors ?? default_colors);
+  const res = await fetch_records(query);
+
+  console.log(JSON.parse(res));
 }
+
+const fetch_records = async (query) => {
+  const base_url = "http://localhost:3000/records";
+  const url = `${base_url}?${query}`;
+  const res = await fetch(url);
+  return res;
+};
 
 const query_builder = (limit, offset, colors) =>
   (
@@ -23,17 +33,11 @@ function test_query_builder() {
   console.log(query_builder(2, false, false));
 }
 
-async function test_retrieve() {
+async function test_fetch_records() {
   await retrieve(
     "http://localhost:3000/records",
     query_builder(2, 2, ["red", "green"])
   );
 }
-
-(async () => {
-  await test_retrieve();
-})();
-
-// test_retrieve();
 
 export default retrieve;
